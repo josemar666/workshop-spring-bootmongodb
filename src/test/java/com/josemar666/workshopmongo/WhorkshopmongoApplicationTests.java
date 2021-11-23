6f482b9136c0a8c05f9cc040235fb7f45a027ff6
josemar666 <josemarlob1@hotmail.com>
2021-11-23 19:21:38 -0300

    primeiro commit - projeto criado

---
+++ src/test/java/com/josemar666/workshopmongo/WhorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josemar666.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
