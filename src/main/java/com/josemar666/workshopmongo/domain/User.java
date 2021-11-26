425f9071d22f41c68b4837ec2d25b06a833cda85
josemar666 <josemarlob1@hotmail.com>
2021-11-26 20:39:41 -0300

    Referenciando os posts do usuário

--- src/main/java/com/josemar666/workshopmongo/domain/User.java
+++ src/main/java/com/josemar666/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.josemar666.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -14,6 +17,9 @@ public class User  implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> post = new ArrayList<>();
 	
 	public User() {
 		
@@ -48,6 +54,15 @@ public class User  implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+
+	public List<Post> getPost() {
+		return post;
+	}
+
+	public void setPost(List<Post> post) {
+		this.post = post;
 	}
 
 	@Override
