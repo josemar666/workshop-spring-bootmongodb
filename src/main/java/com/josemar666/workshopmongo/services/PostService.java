5561863c6d6ac41c98d6f1b03f9075c3724d311b
josemar666 <josemarlob1@hotmail.com>
2021-11-27 15:12:09 -0300

    Consulta simples com @Query

--- src/main/java/com/josemar666/workshopmongo/services/PostService.java
+++ src/main/java/com/josemar666/workshopmongo/services/PostService.java
@@ -23,7 +23,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.findByTitle(text);
 		
 	}
 
